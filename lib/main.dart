8a9e7fe34183817caed6627a56f467c5fe398667
AliRadmanesh <ali.radmanesh1995@gmail.com>
2019-04-29 14:32:34 +0430

    Finish module 2 & 3, understood different debugging tools

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,45 @@
 // Pre-defined functions and tools for creating our application.
 import 'package:flutter/material.dart';
+/*
+## rendering class is usefull for debugging the user interface
+*/
+// import 'package:flutter/rendering.dart';
 
 import './product_manager.dart';
 
 // This is our app's core function, where it starts to launch!
-void main() => runApp(MyApp());
+void main() {
+  /*
+  ## To show each element, its padding & margin, use this:
+  */
+  // debugPaintSizeEnabled = true;
+  /*
+  ## To show where text baseline is, you can enable this:
+  */
+  // debugPaintBaselinesEnabled = true;
+  /*
+  ## To show tapping on each element you can use this var:
+  */
+  // debugPaintPointersEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  // Everything in flutter is widget. even
+  // our whole application is a widget!
+  // We have 2 kind of widgets:
+  // 1. Stateless: Displaying something
+  // 2. Stateful: Can work with date and modify them.
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      /*
+      ## [debugShowMaterialGrid] shows grid in the whole
+      ## page to recognize how much space your element take.
+      */
+      // debugShowMaterialGrid: true,
       title: 'Easy List',
       theme: ThemeData(
         brightness: Brightness.light,
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
@@ -17,6 +18,15 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductManager> {
+  // Also our products list is final, we can add to it.
+  // final means it couldn't be changed. but we can add
+  // to it (modify the exsiting value)! we just can't
+  // assign a new value to it, like _products = ['Ali'];
+  // reason: Objects and lists in general are reference types
+  // and we store a reference to their contents.
+  // Note: In final variables, we can modify them, but
+  // we can't assign a new value to them.
+  // e.g. final age = 25; age.round() is OK!
   List<String> _products = [];
 
   @override
@@ -30,22 +40,26 @@ class _ProductManagerState extends State<ProductManager> {
   }
 
   @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[ProductManager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+      print(_products);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     print('[ProductManager State] build()');
     return Column(
       children: <Widget>[
         Container(
           margin: EdgeInsets.all(10.0),
-          child: RaisedButton(
-            color: Theme.of(context).primaryColor,
-            onPressed: () {
-              setState(() {
-                _products.add('Advanced Food Tester');
-                print(_products);
-              });
-            },
-            child: Text('Add Product'),
-          ),
+          child: ProductControl(_addProduct),
         ),
         Products(_products),
       ],
